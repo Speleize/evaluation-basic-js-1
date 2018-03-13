1d33b49318a2350d9dca9a4e0dfa881a1a266895
Alexandre SUCHOT <alexandre.suchot@ynov.com>
2018-03-13 10:06:40 +0100

    Réalisation de l'exercice 2, partie 2 et 3 fonctionnelles mais pas la 1

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,6 +1,15 @@
 var mapCustomer = function(customer) {
-  // Your future job begins here ...
+var input = {
+firstname: 'Jack',
+lastname: 'Smith',
+products: [{ label: 'shoes', price: 89 }, { label: 'book', price: 14 }, { label: 'smartphone', price: 499 }]
 }
+
+var output = Object.create(input)
+output.moneySpent = 602;
+return output;
+}
+
 
 module.exports = {
   title: 'Exercise 2',
