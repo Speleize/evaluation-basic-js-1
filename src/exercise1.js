fe3e94fff8ace45ecd9cbb3aba3828ba116142ff
Alexandre SUCHOT <alexandre.suchot@ynov.com>
2018-03-13 09:32:18 +0100

    Exercice 1 partie 1 et 2 validé, partie 3 en dessous mais pas validé en même temps

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,6 +1,28 @@
-var removeNegativeValue = function(values) {
-  // Your future job begins here ...
+var removeNegativeValue = function(input) {
+  var input = []
+  var output = []
+  for(i=0;i<input.length;i++){
+    if (input[i]>=0){
+      output.push(input[i])
+    }
+    else {}
+  }
+  return output;
 }
+
+
+
+/*var removeNegativeValue = function(input) {
+  var input = [34, -5, 9, 18, 0, -7, 32]
+  var output = []
+  for(i=0;i<input.length;i++){
+    if (input[i]>=0){
+      output.push(input[i])
+    }
+    else {}
+  }
+  return output;
+}*/
 
 module.exports = {
   title: 'Exercise 1',
